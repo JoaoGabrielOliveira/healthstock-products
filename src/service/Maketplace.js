d6b7e7cad879741a6fffd0432d1052e9b0ca25d2
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-10-17 17:27:30 -0300

    refactor: melhorando endpoint de marketplace

--- src/service/Maketplace.js
+++ src/service/Maketplace.js
@@ -1,7 +1,7 @@
 import SupplierCatalog from "../models/SupplierCatalog.js";
 
 export async function getMarketPlace(req, res, next){
-    const catalog = await SupplierCatalog.find({select: { name: true, description: true, product: true }, relations: { product: true}});
+    const catalog = await SupplierCatalog.find({select: { supplierId:true, name: true, description: true }});
 
     /*res.status(200).send([
         { name:"Siringa", description: "Se", photo: "https://http2.mlstatic.com/D_NQ_NP_856031-MLB47273532229_082021-O.webp" },
